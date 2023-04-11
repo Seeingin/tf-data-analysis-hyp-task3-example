230b54f262133af678c91a2209755e7f3d2e7504
Aleksandra Kuleva <129543447+Seeingin@users.noreply.github.com>
2023-04-11 14:30:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,21 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 752592494 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    from scipy.stats import t
+
+    alpha = 0.02
+    mu = 500
+    n = len(x)
+    x_bar = np.mean(x)
+    s = np.std(x, ddof=1)
+
+    t_stat = (x_bar - mu) / (s / np.sqrt(n))
+    p_val = 1 - t.cdf(t_stat, n-1)
+
+    if p_val < alpha:
+        return True
+    else:
+        return False
